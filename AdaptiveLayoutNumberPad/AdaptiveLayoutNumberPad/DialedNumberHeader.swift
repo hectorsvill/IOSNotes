bf7bd70331aa72b2604a2c1b8f1e9ef33aaf96af
hectorsvill <hvillasano@go.pasadena.edu>
2019-03-27 23:04:12 -0700

    fix repo

--- AdaptiveLayoutNumberPad/AdaptiveLayoutNumberPad/DialedNumberHeader.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DialedNumberHeader.swift
-//  AdaptiveLayoutNumberPad
-//
-//  Created by Hector Steven on 3/27/19.
-//  Copyright © 2019 Hector Steven. All rights reserved.
-//
-
-import Foundation
